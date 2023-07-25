c6e81c809147df4ca7e90c65e16a1cc5a8121e2f
githubmave <lidanmei@yahoo.com>
2023-07-26 00:58:56 +1200

    sfa

--- moderation/index.js
+++ moderation/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require("cors");
 const axios = require('axios');
+// const cors = require("cors");
 
 const app=express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/events",async (req, res) => {
   const {type,data} = req.body;

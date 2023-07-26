8178cc3c186f0514b0a190279666a6f7cf467b84
githubmave <lidanmei@yahoo.com>
2023-07-27 11:31:53 +1200

    add to QuestionCreate

--- client/src/QuestionCreate.tsx
+++ client/src/QuestionCreate.tsx
@@ -2,12 +2,16 @@ import React,{useState} from "react";
 import axios from "axios";
 
 const QuestionCreate = () => {
-    const [question, setQuestion]=useState<string>("");
+    // const [question, setQuestion]=useState<string>("");
+    const [detail,setDetail]=useState<string>("");
     const [id, setId]=useState<number>(0);
     
   const onSubmit =async (event: React.FormEvent<HTMLFormElement>) => {
-     await axios.post<string>("http://localhost:4000/questions",{question,});
-     setQuestion("");
+    //  await axios.post<string>("http://localhost:4000/questions",{question,});
+     await axios.post<string>("http://localhost:4000/questions",{detail,});
+
+    //  setQuestion("");
+    setDetail("");
   };
 
 return(
@@ -16,8 +20,11 @@ return(
       <div className="form-group">
         <label>Interview Questions</label>
         <input  
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
+          // value={question}
+          value={detail}
+          // onChange={(e) => setQuestion(e.target.value)}
+          onChange={(e) => setDetail(e.target.value)}
+
           className="form-control"
           />
       </div>
